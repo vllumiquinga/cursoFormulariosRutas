da8838c379242020299c47d1671c1ce9b5a35447
vllumiquinga <vllumiquinga@gmail.com>
2025-07-04 18:43:25 -0500

    Se aumenta el push

--- src/app/formulario-reactivo/formulario-reactivo.ts
+++ src/app/formulario-reactivo/formulario-reactivo.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-formulario-reactivo',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario-reactivo.html',
   styleUrl: './formulario-reactivo.css'
 })
@@ -12,15 +14,18 @@ export class FormularioReactivo {
   correos:string[] = [];
 
   profileForm = new FormGroup({
-    email: new FormControl('',[Validators.email,Validators.required]),
+    email: new FormControl('',[Validators.required, Validators.email]),
   });
 
   handleSubmit() {
-    alert(
-      this.profileForm.value.email
-    );
-    let correo = this.profileForm.value.email;
-    this.correos.push(correo);
+    if(this.profileForm.valid){
+      this.correos.push(this.profileForm.value.email);
+    }
+    //alert(
+    //  this.profileForm.value.email
+    //);
+    //correo = this.profileForm.value.email;
+    
   }
 
 }
