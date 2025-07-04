a6917f6da00b9945a2508d8778ff3e33902cec5b
vllumiquinga <vllumiquinga@gmail.com>
2025-07-04 18:24:14 -0500

    Se aumenta el formulario reactivo

--- src/app/formulario-reactivo/formulario-reactivo.ts
+++ src/app/formulario-reactivo/formulario-reactivo.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-reactivo.html',
   styleUrl: './formulario-reactivo.css'
 })
 export class FormularioReactivo {
+
+  correos:string[] = [];
+
+  profileForm = new FormGroup({
+    email: new FormControl('',[Validators.email,Validators.required]),
+  });
+
+  handleSubmit() {
+    alert(
+      this.profileForm.value.email
+    );
+    let correo = this.profileForm.value.email;
+    this.correos.push(correo);
+  }
 
 }
